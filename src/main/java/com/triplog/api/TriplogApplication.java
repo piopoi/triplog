68299f6a9d6493849f70c9079615a334b40b4346
piopoi <kangsw1988@gmail.com>
2023-08-29 19:51:31 +0900

    feat: create project

---
+++ src/main/java/com/triplog/api/TriplogApplication.java
@@ -0,0 +1,13 @@
+package com.triplog.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriplogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TriplogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/triplog/api/TriplogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triplog.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriplogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
