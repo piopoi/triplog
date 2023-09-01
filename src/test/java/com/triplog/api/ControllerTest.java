bb0ce21ba21be79648c1eff47c856408c5e5ea98
piopoi <kangsw1988@gmail.com>
2023-09-01 22:41:55 +0900

    test: 테스트 시 데이터베이스 초기화 작업 추가

--- src/test/java/com/triplog/api/ControllerTest.java
+++ src/test/java/com/triplog/api/ControllerTest.java
@@ -1,6 +1,8 @@
 package com.triplog.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.triplog.api.utils.DatabaseCleanup;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
@@ -19,7 +21,13 @@ public class ControllerTest {
 
     @Autowired
     protected MockMvc mockMvc;
-
     @Autowired
     protected ObjectMapper objectMapper;
+    @Autowired
+    protected DatabaseCleanup databaseCleanup;
+
+    @BeforeEach
+    public void basicSetUp() {
+        databaseCleanup.execute();
+    }
 }
