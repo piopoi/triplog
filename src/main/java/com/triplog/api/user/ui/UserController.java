fad33151e709a46116f065c4f6adb6a17d8c10f5
piopoi <kangsw1988@gmail.com>
2023-09-04 13:02:46 +0900

    refactor: UserController의 에러 핸들링 로직 제거
    - 에러 헨들링은 GlobalExceptionHandler로 통합.
    - UserController Test Case 추가

--- src/main/java/com/triplog/api/user/ui/UserController.java
+++ src/main/java/com/triplog/api/user/ui/UserController.java
@@ -1,15 +1,12 @@
 package com.triplog.api.user.ui;
 
-import com.triplog.api.common.error.ErrorResult;
 import com.triplog.api.user.application.UserService;
 import com.triplog.api.user.dto.UserCreateRequest;
 import com.triplog.api.user.dto.UserCreateRequestValidator;
 import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
@@ -33,13 +30,7 @@ public class UserController {
     }
 
     @PostMapping
-    public ResponseEntity createUser(@RequestBody @Validated UserCreateRequest userCreateRequest,
-                                     BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            ErrorResult errorResult = new ErrorResult(bindingResult);
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResult);
-        }
-
+    public ResponseEntity<Void> createUser(@RequestBody @Validated UserCreateRequest userCreateRequest) {
         Long userId = userService.createUser(userCreateRequest);
         return ResponseEntity.created(URI.create("/user/" + userId)).build();
     }
--- src/test/java/com/triplog/api/user/ui/UserControllerTest.java
+++ src/test/java/com/triplog/api/user/ui/UserControllerTest.java
@@ -1,5 +1,6 @@
 package com.triplog.api.user.ui;
 
+import static com.triplog.api.user.constants.UserConstants.*;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
@@ -7,6 +8,7 @@ import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWit
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.restdocs.snippet.Attributes.key;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.triplog.api.ControllerTest;
@@ -16,13 +18,16 @@ import org.junit.jupiter.api.Test;
 
 class UserControllerTest extends ControllerTest {
 
+    private final String email = "test@test.com";
+    private final String password = "12345678";
+
     @Test
     @DisplayName("사용자를 등록할 수 있다.")
     void createUser() throws Exception {
         //given
         UserCreateRequest userCreateRequest = UserCreateRequest.builder()
-                .email("test@test.com")
-                .password("12345678")
+                .email(email)
+                .password(password)
                 .build();
 
         //when then
@@ -41,4 +46,83 @@ class UserControllerTest extends ControllerTest {
                 );
     }
 
+    @Test
+    @DisplayName("이메일 없이 사용자를 등록할 수 없다.")
+    void createUser_emptyEmail() throws Exception {
+        //given
+        UserCreateRequest userCreateRequest = UserCreateRequest.builder()
+                .email("")
+                .password(password)
+                .build();
+
+        //when then
+        mockMvc.perform(post("/api/user")
+                        .accept(APPLICATION_JSON)
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userCreateRequest)))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errors[0].field").value("email"))
+                .andExpect(jsonPath("$.errors[0].message").value(MESSAGE_USER_EMAIL_EMPTY));
+    }
+
+    @Test
+    @DisplayName("잘못된 이메일로 사용자를 등록할 수 없다.")
+    void createUser_invalidEmail() throws Exception {
+        //given
+        UserCreateRequest userCreateRequest = UserCreateRequest.builder()
+                .email("test")
+                .password(password)
+                .build();
+
+        //when then
+        mockMvc.perform(post("/api/user")
+                        .accept(APPLICATION_JSON)
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userCreateRequest)))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errors[0].field").value("email"))
+                .andExpect(jsonPath("$.errors[0].message").value(MESSAGE_USER_EMAIL_INVALID));
+    }
+
+    @Test
+    @DisplayName("비밀번호 없이 사용자를 등록할 수 없다.")
+    void createUser_emptyPassword() throws Exception {
+        //given
+        UserCreateRequest userCreateRequest = UserCreateRequest.builder()
+                .email(email)
+                .password("")
+                .build();
+
+        //when then
+        mockMvc.perform(post("/api/user")
+                        .accept(APPLICATION_JSON)
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userCreateRequest)))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errors[0].field").value("password"))
+                .andExpect(jsonPath("$.errors[0].message").value(MESSAGE_USER_PASSWORD_EMPTY));
+    }
+
+    @Test
+    @DisplayName("8자 미만의 비밀번호로 사용자를 등록할 수 없다.")
+    void createUser_shortPassword() throws Exception {
+        //given
+        UserCreateRequest userCreateRequest = UserCreateRequest.builder()
+                .email(email)
+                .password("123")
+                .build();
+
+        //when then
+        mockMvc.perform(post("/api/user")
+                        .accept(APPLICATION_JSON)
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userCreateRequest)))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errors[0].field").value("password"))
+                .andExpect(jsonPath("$.errors[0].message").value(MESSAGE_USER_PASSWORD_LENGTH_MIN));
+    }
 }
